9b517c331b817d2c85edf0822c372b79cba9a1fe
TheSecondComing123 <78659211+TheSecondComing123@users.noreply.github.com>
2026-03-23 19:09:49 -0400

    feat: add message pane navigation stack for threads/search/pins

--- src/store/state.rs
+++ src/store/state.rs
@@ -1,6 +1,14 @@
 use twilight_model::id::marker::{ChannelMarker, GuildMarker, MessageMarker};
 use twilight_model::id::Id;
 
+#[derive(Debug, Clone)]
+pub enum PaneView {
+    Channel(Id<ChannelMarker>),
+    Thread { parent_channel: Id<ChannelMarker>, thread_id: Id<ChannelMarker> },
+    SearchContext { channel_id: Id<ChannelMarker>, message_id: Id<MessageMarker>, query: String },
+    PinContext { channel_id: Id<ChannelMarker>, message_id: Id<MessageMarker> },
+}
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum FocusTarget {
     ServerList,
@@ -33,6 +41,7 @@ pub struct UiState {
     pub connection_status: ConnectionStatus,
     pub reply_to: Option<ReplyTarget>,
     pub editing_message: Option<Id<MessageMarker>>,
+    pub message_pane_stack: Vec<PaneView>,
 }
 
 #[derive(Debug, Clone)]
@@ -55,6 +64,33 @@ impl Default for UiState {
             connection_status: ConnectionStatus::Connecting,
             reply_to: None,
             editing_message: None,
+            message_pane_stack: Vec::new(),
+        }
+    }
+}
+
+impl UiState {
+    pub fn active_channel(&self) -> Option<Id<ChannelMarker>> {
+        self.message_pane_stack.last().map(|view| match view {
+            PaneView::Channel(id) => *id,
+            PaneView::Thread { thread_id, .. } => *thread_id,
+            PaneView::SearchContext { channel_id, .. } => *channel_id,
+            PaneView::PinContext { channel_id, .. } => *channel_id,
+        }).or(self.selected_channel)
+    }
+
+    pub fn push_pane(&mut self, view: PaneView) {
+        if self.message_pane_stack.len() < 3 {
+            self.message_pane_stack.push(view);
+        }
+    }
+
+    pub fn pop_pane(&mut self) -> bool {
+        if self.message_pane_stack.len() > 1 {
+            self.message_pane_stack.pop();
+            true
+        } else {
+            false
         }
     }
 }
